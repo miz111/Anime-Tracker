4c41d31bd5a230bcc7eebbe543f5fd4777058d47
Tommy Mai <tommymaicollege@gmail.com>
2023-01-17 15:09:56 -0800

    Edit Form

--- ghi/src/AccountEdit.js
+++ ghi/src/AccountEdit.js
@@ -1,64 +1,119 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import { validateEmail, validatePassword } from './validation';
+import { getUserInfo, updateUserInfo } from './api';
 
 const EditAccount = () => {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
   const [error, setError] = useState('');
+  const [success, setSuccess] = useState(false);
+  const [loading, setLoading] = useState(false);
   const history = useHistory();
 
   useEffect(() => {
     // Fetch current user information
     const fetchData = async () => {
-      const data = await getUserInfo();
-      setUsername(data.username);
-      setEmail(data.email);
+      try {
+        const data = await getUserInfo();
+        setUsername(data.username);
+        setEmail(data.email);
+      } catch (err) {
+        setError("Couldn't fetch user information, please try again later.");
+      }
     }
     fetchData();
   }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setError('');
+    if (!validateEmail(email)) {
+      setError("Invalid email address.");
+      return;
+    }
+    if (!validatePassword(password)) {
+      setError("Password must be at least 8 characters long, contains at least one lowercase letter, one uppercase letter and one number.");
+      return;
+    }
+    if (password !== passwordConfirm) {
+      setError("Password confirmation doesn't match.");
+      return;
+    }
+    setLoading(true);
     try {
       // Make a request to update user information
       const data = await updateUserInfo(username, email, password);
       // If update is successful
       if (data.success) {
-        history.push('/profile');
+        setSuccess(true);
+        setTimeout(() => {
+          history.push('/profile');
+        }, 2000);
       } else {
-        setError(data.message);
+        setError(data.message  );
       }
     } catch (err) {
       setError('There was an error updating your account. Please try again.');
+    } finally {
+      setLoading(false);
     }
+  };
+  
+  const handleCancel = () => {
+    history.push('/profile');
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        placeholder="Username"
-        required
-      />
-      <input
-        type="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-        placeholder="Email"
-        required
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Password"
-        required
-      />
-      <button type="submit">Save Changes</button>
-      {error && <p>{error}</p>}
+    <form onSubmit={handleSubmit} className="edit-account-form">
+      <label>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Username"
+          required
+        />
+      </label>
+      <label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          required
+        />
+      </label>
+      <label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          required
+        />
+      </label>
+      <label>
+        <input
+          type="password"
+          value={passwordConfirm}
+          onChange={(e) => setPasswordConfirm(e.target.value)}
+          placeholder="Confirm Password"
+          required
+        /> 
+        </label>
+      <div className="form-buttons">
+        <button type="submit" disabled={loading}>
+          {loading ? 'Saving...' : 'Save Changes'}
+        </button>
+        <button type="button" onClick={handleCancel}>
+          Cancel
+        </button>
+      </div>
+      {error && <p className="error-message">{error}</p>}
+      {success && <p className="success-message">Changes saved successfully!</p>}
     </form>
   );
 };
--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,43 +1,50 @@
 import { useEffect, useState } from "react";
 import Construct from "./Construct.js";
-import HomePage from "./HomePage.js";
-import SignUpForm from "./SignUpForm.js";
 import ErrorNotification from "./ErrorNotification";
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "./Nav";
-// import SignUpForm from "./SignUpForm";
-// import LoginForm from "./LoginForm";
-import { AuthProvider, useToken } from "./auth";
-
+import SignUpForm from "./SignUpForm.js";
+import EditAccount from "./AccountEdit.js";
+import LoginForm from "./LoginForm.js";
+import "./App.css";
 
 
 function App() {
-  const [launch_info, setLaunchInfo] = useState([]);
-  const [error, setError] = useState(null);
+  // const [launch_info, setLaunchInfo] = useState([]);
+  // const [error, setError] = useState(null);
 
-  useEffect(() => {
-    async function getData() {
-      let url = `${process.env.REACT_APP_ACCOUNTS_API_HOST}/api/launch-details`;
-      console.log("fastapi url: ", url);
-      let response = await fetch(url);
-      console.log("------- hello? -------");
-      let data = await response.json();
+  // useEffect(() => {
+  //   async function getData() {
+  //     let url = `${process.env.REACT_APP_ACCOUNTS}/api/launch-details`;
+  //     console.log("fastapi url: ", url);
+  //     let response = await fetch(url);
+  //     console.log("------- hello? -------");
+  //     let data = await response.json();
 
-      if (response.ok) {
-        console.log("got launch data!");
-        setLaunchInfo(data.launch_details);
-      } else {
-        console.log("drat! something happened");
-        setError(data.message);
-      }
-    }
-    getData();
-  }, []);
+  //     if (response.ok) {
+  //       console.log("got launch data!");
+  //       setLaunchInfo(data.launch_details);
+  //     } else {
+  //       console.log("drat! something happened");
+  //       setError(data.message);
+  //     }
+  //   }
+  //   getData();
+  // }, []);
 
   return (
     <div>
-      <ErrorNotification error={error} />
-      <Construct info={launch_info} />
+      {/* <ErrorNotification error={error} />
+      <Construct info={launch_info} /> */}
+      <BrowserRouter>
+        <Nav />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/Top100/" element={<Top100 />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
