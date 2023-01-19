b4327728c00cbfc1c75d45e455a211d149abee92
Alvi Khandakar <alvi.khandakar@gmail.com>
2023-01-18 18:28:19 -0800

    top 100, pretty much complete, needs mapping

--- accounts/queries/accounts.py
+++ accounts/queries/accounts.py
@@ -1,23 +1,134 @@
-import pymongo
-import os
-from bson import ObjectId
+from typing import Optional, Union, List
+from queries.pool import pool
+from pydantic import BaseModel
 
-dbuser = os.environ['MONGO_USER']
-dbpass = os.environ['MONGO_PASSWORD']
-dbhost = os.environ['MONGO_HOST']
-mongodb = os.environ['MONGO_DATABASE']
-mongo_str = f"mongodb://{dbuser}:{dbpass}@{dbhost}"
-client = pymongo.MongoClient(mongo_str)
+class Error(BaseModel):
+    message: str
+
+class DuplicateAccountError(ValueError):
+    pass
+
+class AccountIn(BaseModel):
+    first_name: str
+    last_name: str
+    email: str
+    username: str
+    password: str
+
+class AccountOut(BaseModel):
+    id: int
+    first_name: str
+    last_name: str
+    email: str
+    username: str
+
+class AccountOutWithPassword(AccountOut):
+    hashed_password: str
 
 class AccountQueries:
-    def create_account(self, new_account):
-        db = client[mongodb]
-        result = db.accounts.insert_one(new_account.dict())
-        account = self.get_account_by_id(result.inserted_id)
-        return account
+    def get_one(self, username: str) -> Optional[AccountOutWithPassword]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        SELECT id
+                            , first_name
+                            , last_name
+                            , email
+                            , username
+                            , password
+                        FROM users
+                        WHERE username = (%s)
+                        """,
+                        [username]
+                    )
+                    record = result.fetchone()
+                    print("record", record)
+                    if record is None:
+                        return None
+                    return self.record_to_account_out(record)
+        except Exception as e:
+            print(e)
+            return {"message": "Could not get that account"}
 
-    def get_account_by_id(self, id):
-        db = client[mongodb]
-        result = db.accounts.find_one({ "_id": ObjectId(id) })
-        result['id'] = str(result['_id'])
-        return result
+    def get_all(self) -> Union[Error, List[AccountOut]]:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    db.execute(
+                        """
+                        SELECT id
+                            , first_name
+                            , last_name
+                            , email
+                            , username
+                            , password
+                        FROM users
+                        ORDER BY username;
+                        """
+                    )
+                    return [
+                        AccountOut(
+                            id=record[0],
+                            first_name=record[1],
+                            last_name=record[2],
+                            email=record[3],
+                            username=record[4],
+                        )
+                        for record in db
+                    ]
+        except Exception as e:
+            print(e)
+            return {"message": "Could not get all accounts"}
+
+    def create(self, account: AccountIn, hashed_password: str) -> AccountOutWithPassword:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                        INSERT INTO users
+                            (first_name, last_name, email, username, password)
+                        VALUES
+                            (%s, %s, %s, %s, %s)
+                        RETURNING id;
+                        """,
+                        [
+                            account.first_name,
+                            account.last_name,
+                            account.email,
+                            account.username,
+                            hashed_password,
+                        ]
+                    )
+                    id = result.fetchone()[0]
+                    old_data = account.dict()
+
+                    return AccountOutWithPassword(id=id, hashed_password=hashed_password, **old_data)
+                    # return AccountOutWithPassword(
+                    #     id=record[0],
+                    #     first_name=record[1],
+                    #     last_name=record[2],
+                    #     email=record[3],
+                    #     username=record[4],
+                    #     hashed_password=record[5],
+                    #     )
+
+
+        except Exception:
+            return {"message": "Create did not work"}
+
+    def account_in_to_out(self, id: int, account: AccountIn, hashed_password: str):
+        old_data = account.dict()
+        return AccountOutWithPassword(id=id, hashed_password=hashed_password, **old_data)
+
+    def record_to_account_out(self, record):
+        return AccountOutWithPassword(
+            id=record[0],
+            first_name=record[1],
+            last_name=record[2],
+            email=record[3],
+            username=record[4],
+            hashed_password=record[5],
+        )
--- ghi/src/Top100.js
+++ ghi/src/Top100.js
@@ -6,8 +6,10 @@ import { Link } from "react-router-dom";
 function Top100() {
 	const [scrollHeight, setScrollHeight] = useState(0);
 	const [top100, setTop100] = useState([]);
+  const [genres, setGenres] = useState([]);
 	const [overFlowData, setOverFlowData] = useState(true);
 	const [page, setPage] = useState(1);
+  const blueStar = require('./blueStar.png')
 	const items = [
     {
       src: "https://cdn.myanimelist.net/r/50x70/images/anime/1764/126627.webp?s=08520f4c12e14b0af58ed2ea9fbd328b",
@@ -41,19 +43,100 @@ function Top100() {
     fetchData();
 	}, [page]);
 
+    useEffect(() => {
+      const fetchData = async () => {
+        try {
+          const response = await fetch("https://api.jikan.moe/v4/top/anime");
+          const data = await response.json();
+          if (data.data.length === 0) {
+            setOverFlowData(false);
+            return;
+          }
+          setGenres(data.data.genres.name);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+
+      fetchData();
+    }, [page]);
+
 	console.log(top100)
 
   return (
     <div
-      style={{ overflow: "auto", maxHeight: "200px" }}
-      onScroll={handleScroll}
+    // style={{ overflow: "auto", maxHeight: "500px" }}
+    // onScroll={handleScroll}
     >
       <ul className="results table">
         {top100.map((item, index) => (
-          <li key={index} className="results-item">
-            <div className="item-name">{item.mal_id}</div>
-            <div className="item-price">{item.title}</div>
-            <div className="item-quantity">{item.score}</div>
+          <li key={index} className="card">
+            <div className="row g-0">
+              <div className="col-auto">
+                <span className="Rank">#{item.rank}</span>
+              </div>
+              <div className="col-1">
+                <div className="card-img">
+                  <a href="localhost:3000" target="_blank" rel="noreferrer">
+                    <img
+                      src={item.images.jpg.large_image_url}
+                      alt={item.title}
+                      height="125px"
+                    />
+                  </a>
+                </div>
+              </div>
+              <div className="col-auto"></div>
+              <div className="col-5">
+                <div className="card-body">
+                  <div className="card-title ">
+                    {item.title_english}
+                    <div className="card-subtitle mb-2 text-muted">
+                      {item.title}
+                    </div>
+                  </div>
+                  <div className="badge bg-danger">{item.genres[0].name}</div>
+                </div>
+              </div>
+              <div className="col-auto">
+                <div className="badge bg-warning rounded-pill">
+                  <img
+                    src={blueStar}
+                    alt="score"
+                    height="20px"
+                    align="center"
+                  />
+                  <> </>
+                  {item.score}
+                </div>
+              </div>
+              <div className="col-2">
+                <div className="item-showtype" align="center">
+                  Show Type: {item.type}
+                  <div className="item-episodes">{item.episodes} Episodes</div>
+                </div>
+              </div>
+              <div className="col-2">
+                <div className="item-status" align="center">
+                  {item.status}
+                  <div className="item-lastStatus" align="center">
+                    {item.aired.string.slice(15, -1)}
+                  </div>
+                </div>
+              </div>
+              <div className="col-auto">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-check"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
+                </svg>
+              </div>
+            </div>
           </li>
         ))}
       </ul>
