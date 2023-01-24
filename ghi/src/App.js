a71c6f90a9d0892a670f9ec846c87876e23cd757
Alvi Khandakar <alvi.khandakar@gmail.com>
2023-01-24 12:09:49 -0800

    fixed the app.js, all files are running

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -2,13 +2,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "./Nav";
 import HomePage from "./HomePage.js";
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Favorites from "./Favorites";
 import FavoriteForm from "./FavoriteForm";
 import LoginForm from "./LoginForm";
 import MainPage from "./MainPage";
 import { useToken, AuthProvider, useAuthContext } from "./auth";
-import Nav from "./Nav";
+import Top100 from "./Top100";
+import Details from "./Details";
+import SearchList from "./SearchList";
 
 function GetToken() {
   useToken();
@@ -22,19 +23,18 @@ export default function App() {
   return (
     <div>
       <BrowserRouter>
-        <Nav />
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/Top100/" element={<Top100 />} />
-            <Route path="/:mal_id/" element={<Details />} />
-            <Route path ="/Search/" element={<SearchList />} />
-            <Route path="login/" element={<LoginForm />} />
-            <Route path="favorites" element={<Favorites />} />
-            <Route path="favorites/new" element={<FavoriteForm />} /> */}
-          </Routes>
-        </div>
-      </BrowserRouter>
+        <AuthProvider>
+          <GetToken />
+          <Nav />
+          <div className="container">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/Top100/" element={<Top100 />} />
+              <Route path="/:mal_id/" element={<Details />} />
+              <Route path ="/Search/" element={<SearchList />} />
+              <Route path="login/" element={<LoginForm />} />
+              <Route path="favorites" element={<Favorites />} />
+              <Route path="favorites/new" element={<FavoriteForm />} /> */}
             </Routes>
           </div>
         </AuthProvider>
