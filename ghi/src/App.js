02f22796d0fdcd6170e85737508090903a147461
Chengyun Lee <chengyun.lee2015@gmail.com>
2023-01-24 12:27:00 -0800

    Merge with Main

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -10,34 +10,10 @@ import MainPage from "./MainPage";
 import { useToken, AuthProvider, useAuthContext } from "./auth";
 import Nav from "./Nav";
 
-
 function GetToken() {
   useToken();
   return null;
 }
-
-function App() {
-  const [launch_info, setLaunchInfo] = useState([]);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    async function getData() {
-      let url = `${process.env.REACT_APP_ACCOUNTS_API_HOST}/api/launch-details`;
-      console.log("fastapi url: ", url);
-      let response = await fetch(url);
-      console.log("------- hello? -------");
-      let data = await response.json();
-
-      if (response.ok) {
-        console.log("got launch data!");
-        setLaunchInfo(data.launch_details);
-      } else {
-        console.log("drat! something happened");
-        setError(data.message);
-      }
-    }
-    getData();
-  }, []);
 
 export default function App() {
   const { token } = useAuthContext();
