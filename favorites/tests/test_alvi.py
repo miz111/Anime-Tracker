7f55afdc79ef68faab1b5f72f07593f012298ef1
Alvi Khandakar <alvi.khandakar@gmail.com>
2023-01-24 16:31:07 -0800

    Finished get_all_user_favorites test, stored in favorites microservice/test

--- favorites/tests/test_alvi.py
+++ favorites/tests/test_alvi.py
@@ -5,40 +5,28 @@ from queries.favorites import (
     FavoriteOut,
     FavoriteIn,
     FavoriteRepository,
+    FavoriteList
 )
 from authenticator import authenticator
 from routers import favorites
 
 client = TestClient(app)
 
-class FakeFavoriteRespository:
-    def create(self, favorite):
-        return FavoriteOut(
-            id=1,
-            user_id=favorite.user_id,
-            anime_title=favorite.anime_title,
-            date=favorite.date,
-            img_url=favorite.img_url,
-        )
-
-class FakeAuthenticator:
-    def get_current_account_data(self):
-        return {
-            "id": 1,
-            "first_name": "string",
-            "last_name": "string",
-            "email": "string",
-            "username": "string",
-        }
-
 favorites_db = {
   "favorites": [
     {
-      "id": 0,
-      "user_id": 0
-      "anime_title": "string",
+      "id": 7,
+      "user_id": 0,
+      "anime_title": "Bleach",
       "date": "2023-01-24",
-      "img_url": "string"
+      "img_url": "bleach.png"
+    },
+    {
+      "id": 8,
+      "user_id": 1,
+      "anime_title": "Attack on Titan",
+      "date": "2023-01-24",
+      "img_url": "aot.jpg"
     }
   ]
 }
@@ -46,6 +34,24 @@ favorites_db = {
 def set_favorites_db():
   return favorites_db
 
+class FakeFavoriteRespository:
+    def get_all(self, user_id) -> list[FavoriteOut]:
+      results = []
+      target_list = favorites_db["favorites"]
+      for item in target_list:
+        if item["user_id"] == user_id:
+          results.append(item)
+      return results
+
+class FakeAuthenticator:
+    def get_current_account_data(self):
+        return {
+            "id": 0,
+            "first_name": "string",
+            "last_name": "string",
+            "email": "string",
+            "username": "string",
+        }
 
 
 def test_get_all_favorites():
@@ -53,7 +59,7 @@ def test_get_all_favorites():
   app.dependency_overrides[
         authenticator.get_current_account_data
     ] = FakeAuthenticator
-  response = client.get("/favorites")
+  response = client.get("/favorites/1")
   assert response.status_code == 200
-  assert response.json == [favorites_db]
-  app=.dependency_overrides{}
+  assert response.json()["favorites"][0] == favorites_db["favorites"][1]
+  app.dependency_overrides = {}
