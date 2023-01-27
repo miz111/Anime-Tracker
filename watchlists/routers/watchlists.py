acb23711fd77bd5711c15a25e96464eb16417d7a
Minting <miz111@ucsd.edu>
2023-01-27 11:15:23 -0800

    cleaned up unused code

--- watchlists/routers/watchlists.py
+++ watchlists/routers/watchlists.py
@@ -30,18 +30,6 @@ def get_watchlist(
         return {"watchlists": repo.get_all(user_id)}
 
 
-# get_all for whole database did not work
-# @router.get("/api/watchlists", response_model=WatchlistList)
-# def get_all_watchlist(
-#     repo: WatchlistRepository = Depends(),
-#     account_data: dict = Depends(authenticator.get_current_account_data),
-#     ):
-#     print("GET all watchlists: account data", account_data)
-#     print("GET all watchlists: repo", repo)
-#     if account_data:
-#         return {"watchlists":repo.get_all}
-
-
 @router.delete("/api/watchlists/{user_id}/{watchlist_id}")
 def delete_watchlist(
     user_id: int,
