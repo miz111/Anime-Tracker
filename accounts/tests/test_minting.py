0ca54da5680f50c5489ce657da67ddf5afe99fe7
Minting <miz111@ucsd.edu>
2023-01-27 10:00:18 -0800

    reformatted with black

--- accounts/tests/test_minting.py
+++ accounts/tests/test_minting.py
@@ -4,6 +4,7 @@ from main import app
 
 client = TestClient(app)
 
+
 def test_get_token_for_logged_in_user():
     account = {
         "id": 1,
@@ -12,8 +13,12 @@ def test_get_token_for_logged_in_user():
         "email": "woof@email.com",
         "username": "3meow",
     }
-    app.dependency_overrides[auth.authenticator.try_get_current_account_data] = lambda: account
-    response = client.get("/token", cookies={auth.authenticator.cookie_name: "anime"})
+    app.dependency_overrides[
+        auth.authenticator.try_get_current_account_data
+    ] = lambda: account
+    response = client.get(
+        "/token", cookies={auth.authenticator.cookie_name: "anime"}
+    )
     app.dependency_overrides = {}
     assert response.status_code == 200
     data = response.json()
